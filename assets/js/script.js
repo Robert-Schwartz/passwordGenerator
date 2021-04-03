7de083fb480809888a0aee7e62ae226cc6f31332
Robert Schwartz <bschwartz23@gmail.com>
2021-04-03 11:31:13 -0700

    created button response for prompts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,43 @@
-// Assignment code here
+var lowerCaseChar = ['A','b','c']
+var upperCaseChar = ['A','B','C']
+var numChar = [0,1,2,3,4,5,6,7,8,9]
+var specialChar= ['!','@','#']
 
+
+
+function generatePassword() {
+  var passwordLength = prompt(
+    "Please choose a password length of at least 8 characters and no more than 128 characters"
+  );
+  passwordLength = parseInt(passwordLength);
+  console.log(passwordLength);
+  if (passwordLength < 8 || passwordLength > 128 || passwordLength === NaN) {
+    alert(
+      "That length does not meet the criteria. Please choose a password length of at least 8 characters and no more than 128 characters "
+    );return ;
+  }
+ 
+//pick random characters 
+var userChoices = []
+
+  var confirmLowercase = confirm("Add Lowercase?")
+    if (confirmLowercase) {userChoices = userChoices.concat(lowerCaseChar)};
+//repeat for next three characters 
+
+//#5 add confirm that they did not say no to all of them
+//if all conditions false > return
+
+
+var charactersResult = ""
+
+for (var i = 0; i <passwordLength; i ++) {
+  var singleCharacter = userChoices[Math.floor(Math.random() * userChoices.length)];
+  charactersResult += singleCharacter;
+}
+
+return charactersResult
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,7 +48,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
